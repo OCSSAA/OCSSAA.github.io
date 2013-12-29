37d5cf425b4385393e193c1197b7fa783029d029
Troy Wolters <twwolters@yahoo.ca>
2013-12-29 13:04:20 -0500

    Menu small screen fix

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var open_by_user = false;
+
 $(document).ready(function() {
 	
 	var open = true;
@@ -13,12 +15,13 @@ $(document).ready(function() {
 })
 
 function checkSmaller() {
-	if (window.innerWidth <= 1080 && open) {
+	if (window.innerWidth <= 1080 && open && !open_by_user) {
 		toggle();
 	}
 }
 
 function toggle() {
+	
 	if (open) {
 		$("nav").css({
 			display: "none"
@@ -27,15 +30,35 @@ function toggle() {
 			width: "94%"
 		});
 		open = false;
+		open_by_user = false;
 	}
+	
 	else {
-		$("#wrapper").css({
-			width: "74%"
-		});
+		if (window.innerWidth <= 1080) {
+			$("#wrapper").css({
+				width: "24%"
+			})
+			$("nav").css({
+				width: "70%"
+			})
+		}
+		else {
+			$("#wrapper").css({
+				width: "74%"
+			});
+			$("nav").css({
+				width: "20%"
+			})
+		}
 		setTimeout( function() {
+			
 			$("nav").css({
 				display: "block"
-			})}, 200);
+			})
+
+		}, 200);
+		
 		open = true;
+		open_by_user = true;
 	}
 }
