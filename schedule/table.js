a5712f7cb72c10bc86615ebc73dda1b490a5bb24
Troy Wolters <twwolters@yahoo.ca>
2014-01-09 13:41:50 -0500

    Big Schedule Update

--- schedule/table.js
+++ schedule/table.js
@@ -1,23 +1,55 @@
 var tds = document.getElementsByTagName('td'),
 	selected = "";
 
-for (i in tds) {
-	tds[i].onclick = function() {
-		selected = this.innerHTML;
-		for (n in tds) {
-			// Find all cells with the ID in them
-			if (tds[n].innerHTML.indexOf(selected) != -1) {
-				tds[n].style.background = "rgb(54, 117, 174)";
-				tds[n].style.color = "white";
+function check(str) {
 
-				tds[n].parentElement.children[0].style.background = "rgb(229, 129, 62)";
-				tds[n].parentElement.children[0].style.color = "white";
-			}
-			// Reset the colors each click
-			else {
-				tds[n].style.background = "white";
-				tds[n].style.color = "#333";
-			}
+	var id = /^[A-R]$/;
+	var school = /^((?!Pool).)*$/;
+	var cell = new RegExp("^[A-R] vs " + str[0] + "$|^" + str[0] + " vs [A-R]$|" + "^[A-R] vs " + str[str.length - 1] + "$|^" + str[str.length - 1] + " vs [A-R]$");
+
+	if (id.test(str)) {
+		return "ID";
+	}
+	else if (cell.test(str)) {
+		return "Cell";
+	}
+	else if (school.test(str)) {
+		return "School Name";
+	}
+	else {
+		return false;
+	}
+
+}
+
+function tdsLoop(what) {
+	for (n in tds) {
+		if (check(tds[n].innerHTML) == "Cell" && tds[n].innerHTML.indexOf(what) != -1) {
+			tds[n].style.background = "rgb(54, 117, 174)";
+			tds[n].style.color = "white";
+
+			tds[n].parentElement.children[0].style.background = "rgb(229, 129, 62)";
+			tds[n].parentElement.children[0].style.color = "white";
 		}
+		// Reset the colors each click
+		else {
+			tds[n].style.background = "white";
+			tds[n].style.color = "#333";
+		}
+	}
+}
+
+for (i in tds) {
+	console.log(tds[i].innerHTML);
+	tds[i].onclick = function() {
+		var selected = this.innerHTML;
+		if (this.previousElementSibling !== null) {
+			var	left = this.previousElementSibling.innerHTML;
+		}
+
+		// alert(check(selected));
+
+		check(selected) == "School Name" && tdsLoop(left);
+		check(selected) == "ID" && tdsLoop(selected);
 	}
 }
