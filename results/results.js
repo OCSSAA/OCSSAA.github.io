b72b5e0f7e1ebb042161f879ee1b8631779f8081
Troy Wolters <twwolters@yahoo.ca>
2014-02-01 09:13:08 -0500

    Round 1

--- results/results.js
+++ results/results.js
@@ -1,28 +1,49 @@
 var rows = document.getElementsByTagName('tr');
 
+// function getSize(str) {
+// 	if (str.length == 6 && str[1] == " ") {
+// 		return [1, 4];
+// 	}
+// 	else if (str.length == 6 && str[5] == " ") {
+// 		return [2, 5];
+// 	}
+// 	else {
+// 		return 0;
+// 	}
+// }
+
 for (var i = 0; i < rows.length; i++) {
 	if (rows[i].firstElementChild.innerHTML != "") {
 		var points_for = 0;
 		var points_against = 0;
 		var c = rows[i].children;
 		var points = c[11];
+		var school = c[0].innerHTML;
 
 		for (var n = 1; n <= 6; n++) {
 			if (c[n].children[0] !== undefined) {
-				// console.log(parseInt(c[n].children[0].innerHTML.substr(0,2)));
-				console.log(parseInt(c[n].children[0].innerHTML.substr(5,9)));
-				console.log(parseInt(c[n].children[1].innerHTML.substr(5,9)));
-				if (parseInt(c[n].children[0].innerHTML.substr(0,2)) !== NaN) {
-					points_for += parseInt(c[n].children[0].innerHTML.substr(0,2));
+				console.log(school);
+				var w = parseInt(c[n].children[0].innerHTML.substr(0,2)),
+					x = parseInt(c[n].children[1].innerHTML.substr(0,2)),
+					y = parseInt(c[n].children[0].innerHTML.substr(4)),
+					z = parseInt(c[n].children[1].innerHTML.substr(4));
+
+				console.log(w);
+				console.log(x);
+				console.log(y);
+				console.log(z);
+
+				if (!isNaN(w)) {
+					points_for += w;
 				}
-				if (parseInt(c[n].children[1].innerHTML.substr(0,2)) !== NaN) {
-					points_for += parseInt(c[n].children[0].innerHTML.substr(0,2));
+				if (!isNaN(x)) {
+					points_for += x;
 				}
-				if (parseInt(c[n].children[0].innerHTML.substr(5,8)) !== NaN) {
-					points_against += parseInt(c[n].children[0].innerHTML.substr(4,9));
+				if (!isNaN(y)) {
+					points_against += y;
 				}
-				if (parseInt(c[n].children[1].innerHTML.substr(5,8)) !== NaN) {
-					points_against += parseInt(c[n].children[0].innerHTML.substr(4,9));
+				if (!isNaN(z)) {
+					points_against += z;
 				}
 			}
 		}
